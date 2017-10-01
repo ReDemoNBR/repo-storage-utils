91410e1a4bac65a02e436fcdeb645f0d36d210d5
ReDemoNBR <alanderson_laird@yahoo.com.br>
2017-10-01 14:51:58 -0300

    updated atom packages installed and divided in 2 parts: themes and packages

--- sh-scripts/atom-packages-install.sh
+++ sh-scripts/atom-packages-install.sh
@@ -1,1 +1,7 @@
-apm install atom-beautify atom-html-preview auto-indent autoclose-html autocomplete-modules busy-signal color-picker expose file-icons fonts highlight-selected imdone-atom intentions language-gitignore linter linter-eslint merge-conflicts open-in-browser pigments platformio-ide-terminal script seti-syntax seti-ui tool-bar tool-bar-atom
+echo "Installing packages..."
+apm install atom-beautify atom-html-preview auto-indent autoclose-html autocomplete-modules busy-signal color-picker expose file-icons fonts highlight-selected intentions language-gitignore linter linter-eslint merge-conflicts open-in-browser pigments platformio-ide-terminal script tool-bar tool-bar-atom
+echo "Packages installed"
+echo "Installing themes..."
+apm install hacker-dream-ui hacker-dream-syntax hacking-the-kernel hacking-the-kernel-ui one-dark-shade-ui snow-dark-material-syntax snow-dark-syntax seti-syntax seti-ui
+echo "Themes installed"
+echo "Installing done"
