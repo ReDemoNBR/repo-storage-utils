e0b72c8c8a087b55ff654053ce9efe1dde44b207
ReDemoNBR <alanderson_laird@yahoo.com.br>
2017-12-06 11:56:11 -0200

    automating archlinux-arm configs for RPi3

---
+++ sh-scripts/prepare-arch-with-xfce.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+
+# Make your changes here
+username="myuser"
+userpasswd="mypassword"
+rootpasswd="rootpasswd"
+hostname="myhostname"
+swapfilesize="2G"
+tmpsize="1G"
+keyboard="/usr/share/kbd/keymaps/i386/qwerty/br-abnt2.map.gz"
+mainlang="en_US.UTF-8 UTF-8"
+secondarylangs=("en_US ISO-8859-1" "pt_BR.UTF-8 UTF-8" "pt_BR ISO-8859-1" "en_GB.UTF-8 UTF-8" "en_GB ISO-8859-1")
+timezone="/usr/share/zoneinfo/America/Sao_Paulo"
+
+
+if [[ whoami != "root" ]]; then
+    echo "must run as root"
+    exit 1
+fi
+
+# AS ROOT
+
+## Initialize pacman
+pacman-key --init
+## Refresh pacman
+pacman -Syy
+## Install ArchLinux and ArchLinuxARM keyrings
+pacman -S --needed --noconfirm archlinux-keyring archlinuxarm-keyring
+## Install ca-certificates
+pacman -S --needed --noconfirm ca-certificates ca-certificates-utils ca-certificates-cacert ca-certificates-mozilla
+## Update all packages
+pacman -Syu --noconfirm
+## Install basic packages
+pacman -S --noconfirm base base-devel yajl wget git tk vi vim bash-completion
+## Install raspberry-pi required packages (if not already installed
+pacman -S --needed --noconfirm raspberrypi-firmware raspberrypi-bootloader raspberrypi-bootloader-x
+## Install fake-hwclock as RPi doesn't have RTC
+pacman -S --needed --noconfirm fake-hwclock
+systemctl enable fake-hwclock.service
+
+
+## Configuration
+
+### Keyboard layout
+loadkeys $keyboard
+
+
+### Locale
+#### Uncomment the desires locales in this file ###### nano /etc/locale.gen
+sed -i "/#$mainlang/s/^/#/g" /etc/locale.gen
+for lang in $otherlang; do
+    sed -i "#$lang/s/^/#/g" /etc/locale.gen
+done
+#### Then generate the locales
+locale-gen
+#### configure details in /etc/locale.conf or just add "LANG=en_US.UTF-8" without quotes
+echo "LANG=$(echo $mainlang | cut -d " " -f 1)" > /etc/locale.conf
+
+
+### Timezone
+#### List zoneinfo areas
+######### cd /usr/share/zoneinfo
+######### ls -d */ | grep --color=never ^[A-Z]
+ln -fs $timezone /etc/localtime
+
+
+### Hostname
+#### Create hostname file (using the hostname as "pcname")
+echo $hostname > /etc/hostname
+echo "127.0.1.1 localhost.localdomain $hostname" >> /etc/hosts
+
+
+### Create Swapfile (with 2GB in /swapfile)
+#### Allocate
+fallocate -l $swapfilesize /swapfile
+#### Adjust permissions
+chmod 600 /swapfile
+#### Create swapfile as swap type
+mkswap /swapfile
+#### Activate
+swapon /swapfile
+#### Add it to fstab so it is mounted on boot
+echo "/swapfile none swap defaults 0 0" | tee -a /etc/fstab
+
+### Resize /tmp to 1GB
+echo "tmpfs /tmp tmpfs rw,nodev,nosuid,size=$tmpfilesize 0 0" | tee -a /etc/fstab
+
+
+### Install HRNG driver
+pacman -S --needed --noconfirm rng-tools
+echo 'RNGD_OPTS="-o /dev/random -r /dev/hwrng"' > /etc/conf.d/rngd
+systemctl enable rngd.service
+
+
+## Manage users
+### Create user "username" in wheel group
+useradd --create-home -gid users -groups wheel --shell /bin/bash $username --password="$userpasswd"
+
+### Allow all users from wheel group to execute sudo commands
+EDITOR=nano visudo
+#### and uncomment the wheel group with password
+### Remove the standard user "alarm"
+userdel --remove --force alarm
+### Change root password
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | passwd root
+  $rootpasswd
+  $rootpasswd
+EOF
+
+
+## Login the user "username"
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | su -l $username
+  $userpasswd
+EOF
+
+# AS "username"
+## Create standard folders in home
+cd ~
+mkdir Documents Downloads Musis Pictures Public Templates Videos
+
+## Install yaourt
+mkdir -p /tmp/yaourt
+cd /tmp/yaourt
+### Build dependency package-query
+git clone https://aur.archlinux.org/package-query.git package-query
+cd package-query
+makepkg
+## Login root
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | su -l root
+  $rootpasswd
+EOF
+cd /tmp/yaourt/package-query
+pacman -U package-query*.pkg.tar.xz
+# Root logout
+exit
+
+
+cd /tmp/yaourt
+### Finally build yaourt
+git clone https://aur.archlinux.org/yaourt.git yaourt
+cd yaourt
+makepkg
+## Login root
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | su -l root
+  $rootpasswd
+EOF
+cd /tmp/yaourt/yaourt
+pacman -U yaourt*.pkg.tar.xz
+cd ~
+#### remove these temp files created to install
+rm -fr /tmp/yaourt
+
+
+
+## Install graphical interface
+### Install X.org
+pacman -S --needed --noconfirm xorg-xinit xorg-server xorg-server-common xterm
+### Install video driver
+pacman -S --needed --noconfirm mesa xf86-video-vesa xf86-video-fbdev
+### Install audio
+pacman -S --needed --noconfirm alsa-firmware alsa-utils pulseaudio pulseaudio-alsa
+### Install desktop environment (Xfce)
+pacman -S --needed --noconfirm xfce4 xfce4-goodies exo thunar thunar-archive-plugin thunar-media-tags-plugin xdg-utils xdg-user-dirs
+sudo -u "$username" yaourt -S --noconfirm xdg-su
+### Install packages to automount USB
+pacman -S --needed --noconfirm gvfs udisks2 thunar-volman
+### Install display manager (LightDM GTK Greeter)
+pacman -S --needed --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+systemctl enable lightdm.service
+### Install network manager
+pacman -S --needed --noconfirm networkmanager networkmanager-dispatcher-ntpd networkmanager-openvpn networkmanager-openconnection networkmanager-pptp nm-connection-editor network-manager-applet
+systemctl enable dhcpcd.service
+systemctl enable NetworkManager.service
+### Install bluetooth
+pacman -S --needed --noconfirm blueman bluez bluez-utils
+systemctl enable bluetooth.service
+sudo -u "$username" yaourt -S --noconfirm pi-bluetooth
+systemctl enable brcm43438.service
+### Install pamac (graphical interface to manage packages)
+sudo -u "$username" yaourt -S --noconfirm pamac-aur pamac-tray-appindicator
+
+
+
+## Reboot the system
+shutdown -r now
