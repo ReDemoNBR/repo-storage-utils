d16963d380b458f1114398228d18525a8077ae65
ReDemoNBR <alanderson_laird@yahoo.com.br>
2018-03-05 01:25:54 -0300

    changes

--- sh-scripts/atom-packages-install.sh
+++ sh-scripts/atom-packages-install.sh
@@ -2,7 +2,7 @@ echo "Installing util packages..."
 apm install atom-beautify atom-html-preview busy-signal color-picker expose file-icons fonts intentions merge-conflicts open-in-browser pigments platformio-ide-terminal script tool-bar tool-bar-atom
 echo "Util packages installed"
 echo "Installling coding utils (linters, language syntax, indent, autocomplete etc)"
-apm install auto-indent autoclose-html autocomplete-modules highlight-selected language-gitignore linter linter-eslint linter-shellcheck
+apm install atom-typescript auto-indent autoclose-html autocomplete-modules highlight-selected language-gitignore language-nunjucks linter linter-eslint linter-shellcheck
 echo "Coding utils installed"
 echo "Installing themes..."
 apm install hacker-dream-ui hacker-dream-syntax hacking-the-kernel hacking-the-kernel-ui one-dark-shade-ui snow-dark-material-syntax snow-dark-syntax seti-syntax seti-ui
