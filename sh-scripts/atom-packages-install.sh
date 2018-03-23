8eaf5f48e7731b57d1d4996aaa77b1151637f365
ReDemoNBR <alanderson_laird@yahoo.com.br>
2018-03-23 04:11:22 -0300

    updated atom styles + updated atom packages

--- sh-scripts/atom-packages-install.sh
+++ sh-scripts/atom-packages-install.sh
@@ -1,10 +1,13 @@
 echo "Installing util packages..."
-apm install atom-beautify atom-html-preview busy-signal color-picker expose file-icons fonts intentions merge-conflicts open-in-browser pigments platformio-ide-terminal script tool-bar tool-bar-atom
+apm install busy-signal expose intentions platformio-ide-terminal script tool-bar tool-bar-atom
+apm install atom-beautify atom-html-preview color-picker file-icons fonts merge-conflicts open-in-browser pigments
 echo "Util packages installed"
 echo "Installling coding utils (linters, language syntax, indent, autocomplete etc)"
-apm install atom-typescript auto-indent autoclose-html autocomplete-modules highlight-selected language-gitignore language-nunjucks linter linter-eslint linter-shellcheck
+apm install atom-typescript ide-typescript language-gitignore language-nunjucks language-systemd
+apm install linter linter-eslint linter-flake8 linter-shellcheck
+apm install auto-indent autoclose-html autocomplete-modules highlight-selected 
 echo "Coding utils installed"
 echo "Installing themes..."
-apm install hacker-dream-ui hacker-dream-syntax hacking-the-kernel hacking-the-kernel-ui one-dark-shade-ui snow-dark-material-syntax snow-dark-syntax seti-syntax seti-ui
+apm install one-dark-shade-ui seti-syntax seti-ui
 echo "Themes installed"
 echo "Installing done"
